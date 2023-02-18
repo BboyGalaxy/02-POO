03800ebed602f2ee70216714cf80c1e29457c9a3
adelson <tricker_bboy23@hotmail.com>
2023-02-17 21:44:29 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module 02-POO/course
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"02-POO/course"
+	"fmt"
+)
+
+func main() {
+	Go := course.Course{
+		Name:    "Go desde cero",
+		Price:   12.34,
+		IsFree:  false,
+		UserIDs: []uint{12, 56, 89},
+		Classes: map[uint]string{
+			1: "Introduccion",
+			2: "Estructuras",
+			3: "Maps",
+		},
+	}
+
+	/*css := Course{
+		Name:   "Css desde cero",
+		IsFree: true,
+	}
+
+	js := Course{}
+	js.Name = "Curso JS"
+	js.UserIDs = []uint{12, 67}*/
+
+	fmt.Println(Go.Name)
+	//fmt.Printf("%+v\n", css)
+	//fmt.Printf("%+v", js)
+	Go.PrintClasses()
+	Go.ChangePrice(67.12)
+	fmt.Println(Go.Price)
+
+}
